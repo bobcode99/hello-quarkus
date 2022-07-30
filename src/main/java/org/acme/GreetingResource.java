14bbe0eecfae2e19c5100eb35087fa1c833a963f
yb <lojobs38@gmail.com>
2022-07-30 17:41:26 +0800

    change path

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,19 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
-
+    @ConfigProperty(name = "target", defaultValue = "World")
+    String message;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello " + message + "!";
     }
 }
