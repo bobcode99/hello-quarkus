3eea374f5c5d04351084adcbd7f35a60e3f26fe8
yb <lojobs38@gmail.com>
2022-08-04 10:09:07 +0800

    Add log

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -6,19 +6,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class GreetingResource {
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @ConfigProperty(name = "target", defaultValue = "World")
     String message;
 
     @ConfigProperty(defaultValue = "Students", name="application.greeting.recipient")
     String recipient;
 
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("hello");
+        LOG.debug("done");
+
         return "Hello " + message + "!";
+
     }
 
 
