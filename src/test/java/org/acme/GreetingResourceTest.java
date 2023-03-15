3c01c8c07c8042c7268e8dc8fc873f2f79b4b6da
yb <lojobs38@gmail.com>
2023-03-15 12:12:41 +0800

    Update unit test

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -12,7 +12,7 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/")
+          .when().get("/example/hello/World")
           .then()
              .statusCode(200)
              .body(is("Hello World!"));
